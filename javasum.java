09b6daf24ab52c119c225bf4d896ccbf52934892
sumit970 <sumitfy@gmail.com>
2022-09-02 22:23:56 +0530

    initial commit

---
+++ javasum.java
@@ -0,0 +1,10 @@
+public  class javasum{
+    public static void main(String[] args) {
+        
+        int sum=0;
+        int a=10,b=20;
+        sum=a+b;
+        System.out.println(sum);
+
+    }
+}
